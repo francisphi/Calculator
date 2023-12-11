ea3fb3184c2c86d87b67db608a3f648c80b5e5a3
Jess_francis_Aisen <128569963+francisphi@users.noreply.github.com>
2023-12-11 22:51:28 +0800

    Add files via upload

---
+++ javascript/login.js
@@ -0,0 +1,56 @@
+   function login(event) {
+      
+
+         let login=document.getElementById("email").value;
+               let password=document.getElementById("password").value;
+
+string=" ";
+
+               if((login==="admin") && (password==="admin")){
+              window.location.href="index.html";
+                 
+                  
+               }
+               else if((login!=="admin")&& ( password==="admin"))
+               {
+                  Swal.fire({
+                 title: "Error!",
+                 text: "Incorrect UserName!. Plss Try Agian." ,
+                 icon: "error"
+});
+               }
+               else if((login==="admin")&& ( password!=="admin"))
+               {
+                 Swal.fire({
+                 title: "Error!",
+                 text: "Incorrect Password!. Plss Try Again",
+                 icon: "error"
+});
+               }
+               
+               else
+               {
+                 Swal.fire({
+                 title: "Error!",
+                 text: "Incorrect UserName & Password!. Plss Try Again",
+                 icon: "error"
+});
+               }
+
+               event.preventDefault();
+            }
+            
+
+function PasswordVisibility() { 
+const passwordInput = document.getElementById('password'); 
+const eyeIcon = document.querySelector('.password-toggle i');
+ if (passwordInput.type === 'password') { 
+ passwordInput.type = 'text';
+  eyeIcon.classList.remove('fa-eye-slash'); 
+ eyeIcon.classList.add('fa-eye'); } 
+ else { 
+ passwordInput.type = 'password';
+  eyeIcon.classList.remove('fa-eye');
+   eyeIcon.classList.add('fa-eye-slash');
+
+ } }
